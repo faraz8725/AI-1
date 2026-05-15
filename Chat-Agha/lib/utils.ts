f831c8e8d8c4d1b5cb7a681c8c507a8a16867841
Faraz Sarfaraz Agha <fagha4697@gmail.com>
2026-05-15 07:42:23 +0530

    commits

--- Chat-Agha/lib/utils.ts
+++ Chat-Agha/lib/utils.ts
@@ -12,3 +12,17 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
